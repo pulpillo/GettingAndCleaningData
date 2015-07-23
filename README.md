613f1b3aeaefa67c748864980152a2ca935a3544
pulpillo <juanpablo.lopez.pulpillo@gmail.com>
2015-07-23 07:16:42 +0200

    Add comments

--- README.md
+++ README.md
@@ -6,16 +6,17 @@ The folder data contains the dta files using in this project, previously downloa
 
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
+I change the order of some steps in order to clarify the algorithm.
+
 The R script, `run_analysis.R`, does the following:
 
-1. Load the activity and feature info
-2. Loads both the training and test datasets, keeping only those columns which
-   reflect a mean or standard deviation
-3. Loads the activity and subject data for each dataset, and merges those
-   columns with the dataset
-4. Merges the two datasets
-5. Converts the `activity` and `subject` columns into factors
-6. Creates a tidy dataset that consists of the average (mean) value of each
-   variable for each subject and activity pair.
+1. Extract from the fetures.txt file the measurements on the mean and extandard deviation to subset the data sets
+2. Merges the training and test sets to create one data set subsetting with the features from the STEP 1
+3. Use descriptive features names to name the features in the data set
+   Use Descriptive actitivy names to name the activities in the data set
+   Appropiately labels the data set with descriptive variable names
+4. From the data set in step 4, creates a second, independent tidy data set with the average 
+   of each variable for each activity and each subject.
+
 
 The end result is shown in the file `tidy.txt`.
--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,11 @@ library(reshape2)
 #path to the data
 dataPath  = "data"
 
-#Load Features before to subset training and test dataset
+# STEP 1
+# Extract from the fetures.txt file the measurements on the mean and extandard deviation to subset the data sets
+################################################################################################################
+
+# Load Features before to subset training and test dataset
 dtFeatures <- read.table(file.path(dataPath,"features.txt"))
 dtFeatures[,2] <- as.character(dtFeatures[,2])
 featuresWanted <- grep(".*mean.*|.*std.*", dtFeatures[,2])
@@ -12,13 +16,17 @@ featuresWanted.names <- gsub('-mean', 'Mean', featuresWanted.names)
 featuresWanted.names <- gsub('-std', 'Std', featuresWanted.names)
 featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
 
-#Load Training Data
+# STEP 2
+# Merges the training and test sets to create one data set subsetting with the features from the STEP 1
+########################################################################################################
+
+# Load Training Data
 train <- read.table(file.path(dataPath, "train","x_train.txt"))[featuresWanted] #Subset with dev and mean fields
 trainActivities <- fread(file.path(dataPath, "train","y_train.txt"))
 trainSubjects <- fread(file.path(dataPath, "train", "subject_train.txt"))
 train <- cbind(trainSubjects, trainActivities, train)
 
-#Load Test Data
+# Load Test Data
 test <- read.table(file.path(dataPath, "test","x_test.txt"))[featuresWanted] #Subset with dev and mean fields
 testActivities  <- fread(file.path(dataPath, "test","y_test.txt"))
 testSubjects  <- fread(file.path(dataPath, "test" , "subject_test.txt" ))
@@ -27,10 +35,16 @@ test <- cbind(testSubjects, testActivities, test)
 # Merge Data Sets
 data <- rbind(train, test)
 
-#Add Labels
+# STEP 3
+# Use descriptive features names to name the features in the data set
+# Use Descriptive actitivy names to name the activities in the data set
+# Appropiately labels the data set with descriptive variable names
+#######################################################################
+
+# Add Features Labels
 setNames(data,c("subject", "activity", featuresWanted.names))
 
-#Load ACtivity Labels
+# Add ACtivity Labels
 activityLabels <- read.table(file.path(dataPath,"activity_labels.txt"))
 activityLabels[,2] <- as.character(activityLabels[,2])
 
@@ -38,10 +52,16 @@ activityLabels[,2] <- as.character(activityLabels[,2])
 data$activity <- factor(data$activity, levels = activityLabels[,1], labels = activityLabels[,2])
 data$subject <- as.factor(data$subject)
 
-#Melt the data by subject and activity
+# STEP 4 
+# From the data set in step 4, creates a second, independent tidy data set with the average 
+# of each variable for each activity and each subject.
+###########################################################################################
+
+# Melt the data by subject and activity
 data.melted <- melt(data, id = c("subject", "activity"))
 
-#Prepary the tidy data set
+# Prepary the tidy data set, calculate the mean
 data.tidy <- dcast(data.melted, subject + activity ~ variable, mean)
 
+# Write tidy dataset to tidy.txt fiel
 write.table(data.tidy, "tidy.txt", row.names = FALSE, quote = FALSE)
